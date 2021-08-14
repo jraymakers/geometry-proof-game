1ec440e2a7ea09e674d83ececab83599ecd82c67
Jeff Raymakers <jephly@gmail.com>
2021-08-13 20:43:31 -0700

    experimental type for proof by cases

--- src/E2/types/Proof.tsx
+++ src/E2/types/Proof.tsx
@@ -11,3 +11,20 @@ export type ProvedAssertion = Readonly<{
 }>;
 
 export type ProvedAssertionList = readonly ProvedAssertion[];
+
+
+
+// export type ProofByCases = Readonly<{
+  
+//   mutualConsequents: ProvedByCasesAssertionList;
+// }>;
+
+export type ProvedByCasesAssertion = Readonly<{
+  // is the assertion that is either positive or negative needed?
+  positiveProof: Proof;
+  negativeProof: Proof;
+  positiveIndex: number;
+  negativeIndex: number;
+}>;
+
+export type ProvedByCasesAssertionList = readonly ProvedByCasesAssertion[];
